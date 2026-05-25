516bb295ef3cffbd0046052c01be9a5c9b6cff5f
Stefanus Airlangga Putra Widyantoro <st.airlangga.pw30@gmail.com>
2026-05-25 20:51:11 +0700

    coffeshop

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import navbar from './components/Navbar'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
